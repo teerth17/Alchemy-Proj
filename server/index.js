2dff3373a86fcaed8ac537939e112f37daeef1aa
Teerth Patel <pteerth989@gmail.com>
2023-03-18 23:12:47 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { recoverPublicKey } = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { sha256 } = require("ethereum-cryptography/sha256");
+const {
+  hexToBytes,
+  toHex,
+  utfToBytes,
+} = require("ethereum-cryptography/utils");
+
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0xf847EF00fb884f1d24Ef82c6594557490c998F89": 100,
+  "0x1958cd5ba9302e9c067e56d3bca705e67768316e": 50,
+  "0xeb77dc8bfec7bc141203efb3f055ee4069c0bb3a": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,11 +28,33 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, signature, recoverKey, msgHash, nonce } =
+    req.body;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
+  let senderPublicKey = recoverPublicKey(
+    hexToBytes(msgHash),
+    hexToBytes(signature),
+    recoverKey
+  );
+
+  let senderAddress = `0x${toHex(
+    keccak256(senderPublicKey.slice(1)).slice(12)
+  )}`;
+  if (senderAddress != sender) {
+    res.status(401).send({ message: "Not Authorized Sender" });
+  }
+
+  let msg = { sender, recipient, amount, nonce };
+  let newCalHash = toHex(sha256(utfToBytes(JSON.stringify(msg))));
+
+  if (newCalHash != msgHash) {
+    res.status(400).send({
+      message: "Invalid Hash",
+    });
+  }
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
